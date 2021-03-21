fec999b3ec092e6f7bf2943bed21ddda509a786f
musarrat77 <79351350+musarrat77@users.noreply.github.com>
2021-03-21 18:43:33 +0530

    Add files via upload

---
+++ n.py
@@ -0,0 +1,4 @@
+from userbot.utils import admin_cmd 
+@borg.on(admin_cmd(pattern="n"))
+async def xdcuteboy(event):
+  await event.edit('''Helo I Am an new userbot that is now going to be made by our group members''')
